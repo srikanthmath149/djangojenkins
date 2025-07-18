14eed5bf3567ce05e39964c06aee2094b34a005e
Srikanth <srikanthmath149@gmail.com>
2025-07-18 12:00:22 +0530

    Changes in the envsetup

--- envsetup.sh
+++ envsetup.sh
@@ -13,12 +13,11 @@ source env/bin/activate
 pip3 install -r requirements.txt
 
 
-if [-d "logs"]  then
+if [-d "logs"];  then
     echo "Logs directory already exists."
 else
     mkdir logs
-    touch logs/error.log
-    touch logs/info.log
+    touch logs/error.log logs/info.log
 fi
 
 sudo chmod -R 777 logs
