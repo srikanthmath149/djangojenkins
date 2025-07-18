5b7d7e7895857a66cb15c52d299f0582fe783f7b
Srikanth <srikanthmath149@gmail.com>
2025-07-18 12:11:58 +0530

    Changes in the env and gunicorn setup files

--- envsetup.sh
+++ envsetup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ -d "env" ]; then
+if [ -d "venv" ]; then
     echo "Python virtual environment already exists."
 else
-    python3 -m venv env   
+    python3 -m venv venv   
 fi
 
 echo $PWD
 
-source env/bin/activate
+source venv/bin/activate
 
 pip3 install -r requirements.txt
 
